4f895cd2d6fbbd3a37f59f1263fe71273b10126c
ChoppyChoppa <nonthananhinmalai@gmail.com>
2026-02-02 18:00:36 +0700

    Completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,12 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int& a,int& b,int& c,int& d){
+	int *p[4] = {&a,&b,&c,&d};
+	for (int i=3;i>0;i--){
+		int j = rand() % (i+1);
+		int temp = *p[i];
+		*p[i] = *p[j];
+		*p[j] = temp;
+	}
+}
